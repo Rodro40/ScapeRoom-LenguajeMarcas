5b8b2116c4a64fa5f6040422ac886961184bae12
RickRD14 <153659398+RickRD14@users.noreply.github.com>
2024-02-10 00:24:33 +0100

    Que siga otro xdddddddd, es broma

--- js/boostrap.js
+++ js/boostrap.js
@@ -14,23 +14,28 @@ function getLinterna() {
 
 
 function entrarE2() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne2.png)";
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene2.png)";
     document.getElementById("puertaE1").style.display = "none";
     document.getElementById("puertaE9").style.display = "none";
+    document.getElementById("puertaE7").style.display = "none";
+    document.getElementById("linternaE7").style.display = "none";
     document.getElementById("puertaE2").style.display = "block";
 }
 
 function entrarE3() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne3.png)";
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene3.png)";
     document.getElementById("puertaE2").style.display = "none";
-    document.getElementById("puertaE12").style.display = "none";
-    document.getElementById("puertaE15").style.display = "none";
+    document.getElementById("puertaE9").style.display = "none";
+    document.getElementById("papelE9").style.display = "none";
+    document.getElementById("candadoE9").style.display = "none";
+    document.getElementById("puertaAtrasE9").style.display = "none";
+    document.getElementById("puertaE10").style.display = "none";
     document.getElementById("puertaizqE3").style.display = "block";
     document.getElementById("puertadchaE3").style.display = "block";
 }
 
 function entrarE4() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne4.png)";
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene4.jpeg)";
     document.getElementById("puertaizqE3").style.display = "none";
     document.getElementById("puertadchaE3").style.display = "none";
     document.getElementById("puertaRoja").style.display = "block";
@@ -38,43 +43,49 @@ function entrarE4() {
 }
 
 function entrarE5() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne5.png)";
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene5.jpeg)";
     document.getElementById("puertaRoja").style.display = "none";
     document.getElementById("puertaAzul").style.display = "none";
     document.getElementById("puertaE5").style.display = "block";
-    if (key = false) {
+    if (key == false) {
         document.getElementById("llaveE5").style.display = "block";
     }
 }
 
-function entrarE8() {
-    if (condition) {
-        document.getElementById("principal").style.backgroundImage = "url(../img/Scenne8.png)";
+function entrarE6() {
+    if (key == true) {
+        document.getElementById("principal").style.backgroundImage = "url(./img/Scene6.jpeg)";
         document.getElementById("puertaE5").style.display = "none";
-        document.getElementById("puertaE8").style.display = "block";
+        document.getElementById("puertaE6").style.display = "block";
     } else {
         document.getElementById("msg").style.display = "block";
         document.getElementById("msg").innerHTML = "Mmmmmm, necesito una llave para abrir la puerta";
     }
 }
 
-function entrarE9() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne9.png)";
+function entrarE7() {
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene7.jpeg)";
     document.getElementById("puertaAzul").style.display = "none";
-    document.getElementById("puertaE9").style.display = "block";
-    if (linterna = false) {
-        document.getElementById("linterna").style.display = "block";
+    document.getElementById("puertaRoja").style.display = "none";
+    document.getElementById("puertaE7").style.display = "block";
+    if (linterna == false) {
+        document.getElementById("linternaE7").style.display = "block";
     }
 }
 
-function entrarE11() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne11.png)";
+function entrarE8() {
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene8.jpeg)";
     document.getElementById("puertaizqE3").style.display = "none";
     document.getElementById("puertadchaE3").style.display = "none";
-    document.getElementById("puertaE11").style.display = "block"
+    document.getElementById("puertaE6").style.display = "none";
+    document.getElementById("puertaE8").style.display = "block"
 }
 
-function entrarE12() {
-    document.getElementById("principal").style.backgroundImage = "url(../img/Scenne12.png)";
-
+function entrarE9() {
+    document.getElementById("principal").style.backgroundImage = "url(./img/Scene9.jpeg)";
+    document.getElementById("puertaE8").style.display = "none";
+    document.getElementById("puertaE9").style.display = "block"
+    document.getElementById("puertaAtrasE9").style.display = "block"
+    document.getElementById("papelE9").style.display = "block"
+    document.getElementById("candadoE9").style.display = "block";
 }
